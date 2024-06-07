f841166393ac5a8c4d3acbc22147bf2ac6dfc5c1
Tomasz Janas <tomek@janas.dev>
2024-06-07 21:28:10 +0200

    Swap ctrl and shift, replace arrows with parentheses

--- keyboards/ferris/keymaps/tjanas94/keymap.c
+++ keyboards/ferris/keymaps/tjanas94/keymap.c
@@ -13,19 +13,14 @@ enum layers {
     _ONE_SHOT_HACK
 };
 
-enum custom_keycodes {
-    FAT_ARROW_MACRO = SAFE_RANGE,
-    ARROW_MACRO
-};
-
 #define KC_GUIA LGUI_T(KC_A)
 #define KC_ALTR LALT_T(KC_R)
-#define KC_SHFS LSFT_T(KC_S)
-#define KC_CTRT LCTL_T(KC_T)
+#define KC_CTLS LCTL_T(KC_S)
+#define KC_SHFT LSFT_T(KC_T)
 #define KC_HYPG ALL_T(KC_G)
 #define KC_HYPM ALL_T(KC_M)
-#define KC_CTRN RCTL_T(KC_N)
-#define KC_SHFE RSFT_T(KC_E)
+#define KC_SFTN RSFT_T(KC_N)
+#define KC_CTLE RCTL_T(KC_E)
 #define KC_ALTI LALT_T(KC_I)
 #define KC_GUIO RGUI_T(KC_O)
 #define KC_ALTX RALT_T(KC_X)
@@ -59,12 +54,12 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case KC_GUIA:
         case KC_ALTR:
-        case KC_SHFS:
-        case KC_CTRT:
+        case KC_CTLS:
+        case KC_SHFT:
         case KC_HYPG:
         case KC_HYPM:
-        case KC_CTRN:
-        case KC_SHFE:
+        case KC_SFTN:
+        case KC_CTLE:
         case KC_ALTI:
         case KC_GUIO:
         case KC_LSPC:
@@ -82,38 +77,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         is_double_tap = false;
     }
 
-    const uint8_t mods = get_mods();
-    const uint8_t oneshot_mods = get_oneshot_mods();
-
     switch (keycode) {
-        case FAT_ARROW_MACRO:
-            if (record->event.pressed) {
-                if ((mods | oneshot_mods) & MOD_BIT(KC_LSFT)) {
-                    clear_oneshot_mods();
-                    unregister_mods(MOD_MASK_CSAG);
-                    SEND_STRING("<=");
-                    register_mods(mods);
-                } else {
-                    SEND_STRING("=>");
-                }
-            }
-
-            return false;
-
-        case ARROW_MACRO:
-            if (record->event.pressed) {
-                if ((mods | oneshot_mods) & MOD_BIT(KC_LSFT)) {
-                    clear_oneshot_mods();
-                    unregister_mods(MOD_MASK_CSAG);
-                    SEND_STRING("<-");
-                    register_mods(mods);
-                } else {
-                    SEND_STRING("->");
-                }
-            }
-
-            return false;
-
         case KC_OSFT:
             if (record->event.pressed) {
                 if (record->tap.count > 0) {
@@ -149,8 +113,8 @@ enum combos {
     TAB_COMBO,
     EQL_COMBO,
     MINS_COMBO,
-    FAT_ARROW_COMBO,
-    ARROW_COMBO,
+    LPRN_COMBO,
+    RPRN_COMBO,
     LBRC_COMBO,
     RBRC_COMBO
 };
@@ -159,8 +123,8 @@ const uint16_t PROGMEM esc_combo[] = {KC_H, KC_COMM, COMBO_END};
 const uint16_t PROGMEM tab_combo[] = {KC_C, KC_D, COMBO_END};
 const uint16_t PROGMEM eql_combo[] = {KC_ALTX, KC_C, COMBO_END};
 const uint16_t PROGMEM mins_combo[] = {KC_COMM, KC_ALTD, COMBO_END};
-const uint16_t PROGMEM fat_arrow_combo[] = {KC_W, KC_F, COMBO_END};
-const uint16_t PROGMEM arrow_combo[] = {KC_F, KC_P, COMBO_END};
+const uint16_t PROGMEM lprn_combo[] = {KC_W, KC_F, COMBO_END};
+const uint16_t PROGMEM rprn_combo[] = {KC_F, KC_P, COMBO_END};
 const uint16_t PROGMEM lbrc_combo[] = {KC_L, KC_U, COMBO_END};
 const uint16_t PROGMEM rbrc_combo[] = {KC_U, KC_Y, COMBO_END};
 
@@ -169,8 +133,8 @@ combo_t key_combos[] = {
     [TAB_COMBO] = COMBO(tab_combo, KC_TAB),
     [EQL_COMBO] = COMBO(eql_combo, KC_EQL),
     [MINS_COMBO] = COMBO(mins_combo, KC_MINS),
-    [FAT_ARROW_COMBO] = COMBO(fat_arrow_combo, FAT_ARROW_MACRO),
-    [ARROW_COMBO] = COMBO(arrow_combo, ARROW_MACRO),
+    [LPRN_COMBO] = COMBO(lprn_combo, KC_LPRN),
+    [RPRN_COMBO] = COMBO(rprn_combo, KC_RPRN),
     [LBRC_COMBO] = COMBO(lbrc_combo, KC_LBRC),
     [RBRC_COMBO] = COMBO(rbrc_combo, KC_RBRC)
 };
@@ -178,42 +142,42 @@ combo_t key_combos[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x5_2(
         KC_Q,       KC_W,       KC_F,       KC_P,       KC_B,       KC_J,       KC_L,       KC_U,       KC_Y,       KC_QUOT,
-        KC_GUIA,    KC_ALTR,    KC_SHFS,    KC_CTRT,    KC_HYPG,    KC_HYPM,    KC_CTRN,    KC_SHFE,    KC_ALTI,    KC_GUIO,
+        KC_GUIA,    KC_ALTR,    KC_CTLS,    KC_SHFT,    KC_HYPG,    KC_HYPM,    KC_SFTN,    KC_CTLE,    KC_ALTI,    KC_GUIO,
         KC_Z,       KC_ALTX,    KC_C,       KC_D,       KC_V,       KC_K,       KC_H,       KC_COMM,    KC_ALTD,    KC_SLSH,
                                             KC_OSFT,    KC_LSPC,    KC_LBSP,    KC_LENT
     ),
 
     [_NAV] = LAYOUT_split_3x5_2(
         KC_NO,      KC_BACK,    KC_FRWD,    KC_NO,      KC_NO,      KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     KC_CAPS,
-        KC_LGUI,    KC_LALT,    KC_LSFT,    KC_LCTL,    KC_NO,      KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    KC_INS,
+        KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    KC_NO,      KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    KC_INS,
         KC_UNDO,    KC_CUT,     KC_COPY,    KC_SEL,     KC_PAST,    KC_H,       KC_J,       KC_K,       KC_L,       KC_APP,
                                             KC_NO,      KC_NO,      KC_BSPC,    KC_DEL
     ),
 
     [_NUM] = LAYOUT_split_3x5_2(
         KC_UNDS,    KC_9,       KC_8,       KC_7,       KC_EQL,     KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
-        KC_MINS,    KC_6,       KC_5,       KC_4,       KC_ASTR,    KC_NO,      KC_LCTL,    KC_LSFT,    KC_LALT,    KC_LGUI,
+        KC_MINS,    KC_6,       KC_5,       KC_4,       KC_ASTR,    KC_NO,      KC_LSFT,    KC_LCTL,    KC_LALT,    KC_LGUI,
         KC_PLUS,    KC_3,       KC_2,       KC_1,       KC_SLSH,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
                                             KC_DOT,     KC_0,       KC_NO,      KC_NO
     ),
 
     [_PAR] = LAYOUT_split_3x5_2(
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_COMM,    KC_COLN,    KC_SCLN,    KC_DQUO,
-        KC_LGUI,    KC_LALT,    KC_LSFT,    KC_LCTL,    KC_NO,      KC_NO,      KC_LPRN,    KC_LCBR,    KC_RCBR,    KC_RPRN,
+        KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    KC_NO,      KC_NO,      KC_LPRN,    KC_LCBR,    KC_RCBR,    KC_RPRN,
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_LT,      KC_LBRC,    KC_RBRC,    KC_GT,
                                             KC_NO,      KC_NO,      KC_NO,      KC_FUNC
     ),
 
     [_SYM] = LAYOUT_split_3x5_2(
         KC_UNDS,    KC_BSLS,    KC_PIPE,    KC_AMPR,    KC_EQL,     KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
-        KC_TILD,    KC_CIRC,    KC_PERC,    KC_DLR,     KC_ASTR,    KC_NO,      KC_LCTL,    KC_LSFT,    KC_LALT,    KC_LGUI,
+        KC_TILD,    KC_CIRC,    KC_PERC,    KC_DLR,     KC_ASTR,    KC_NO,      KC_LSFT,    KC_LCTL,    KC_LALT,    KC_LGUI,
         KC_GRV,     KC_HASH,    KC_AT,      KC_EXLM,    KC_SLSH,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
                                             KC_FUNC,    KC_NO,      KC_NO,      KC_NO
     ),
 
     [_FUNC] = LAYOUT_split_3x5_2(
         KC_F12,     KC_F9,      KC_F8,      KC_F7,      KC_SCRL,    KC_NO,      KC_MSTP,    KC_MPLY,    KC_MUTE,    KC_NO,
-        KC_F11,     KC_F6,      KC_F5,      KC_F4,      KC_PSCR,    KC_BRIU,    KC_LCTL,    KC_LSFT,    KC_LALT,    KC_LGUI,
+        KC_F11,     KC_F6,      KC_F5,      KC_F4,      KC_PSCR,    KC_BRIU,    KC_LSFT,    KC_LCTL,    KC_LALT,    KC_LGUI,
         KC_F10,     KC_F3,      KC_F2,      KC_F1,      KC_PAUS,    KC_BRID,    KC_MPRV,    KC_VOLD,    KC_VOLU,    KC_MNXT,
                                             KC_NO,      KC_NO,      KC_NO,      KC_NO
     )
